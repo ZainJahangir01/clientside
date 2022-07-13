4edb61df043c7880081cbb62208c45d00611f370
Zain Sheikh <70570850+ZainJahangir01@users.noreply.github.com>
2022-07-13 16:06:04 +0500

    MernStack UI
    client side of the Student Registration Terminal.

---
+++ src/Components/showStudent/showStudent.js
@@ -0,0 +1,64 @@
+import React, {useEffect , useState} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { makeStyles } from '@material-ui/core';
+import axios from 'axios';
+
+const useStyles = makeStyles({
+    table:{
+        minWidth: 650,
+    },
+});
+
+
+
+export default function ShowStudent() {
+    const classes = useStyles();
+
+    const [studentsList , setStudentList] = useState([])
+    useEffect(() => {
+      axios.get('http://localhost:5000/students').then( (allstudents) => {
+        setStudentList(allstudents.data);
+      })
+     
+    },[])
+  return (
+      <>
+      <h2> All Students </h2>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Registration Number</TableCell>
+            <TableCell align="right">Grade</TableCell>
+            <TableCell align="right">Section</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {studentsList.map((student , key) => (
+            <TableRow
+              key={key}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {student.studentName}
+              </TableCell>
+              <TableCell align="right">{student.regNo}</TableCell>
+              <TableCell align="right">{student.grade}</TableCell>
+              <TableCell align="right">{student.section}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+  );
+}
